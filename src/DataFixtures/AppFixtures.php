21ec579865a3488bbbbf730723bc5cdbe95ce2c8
Sacha Solfer <sachafernandezsoltane@gmail.com>
2022-11-30 11:47:34 +0100

    Adding fixtures exemple

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -18,13 +18,17 @@ class AppFixtures extends Fixture
         $categEX = new Categorie(5,"Exploitations");
 
         $bien1 = new Bien(1,"Activités Equestres, Apiculture, Chasse","Propriété Charente-Maritime","0","330000","St sulpice de royan","17200","17",$categEX,"17.03.017");
+        $bien2 = new Bien(2,"FERME 100% HERBAGERE/ ELEVAGE LAITIER","Située à l'orée d'un bourg, à 10 minutes des services et commerces.","1","950","Rennes","35200","34",$categEX,"19.07.118");
 
         $manager->persist($categTA);
         $manager->persist($categPR);
         $manager->persist($categBO);
         $manager->persist($categBA);
         $manager->persist($categEX);
+
         $manager->persist($bien1);
+        $manager->persist($bien2);
+
 
 
         $manager->flush();
