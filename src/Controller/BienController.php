dbb6fdb142489f72f0b0e251ea9c6a6795f90d50
Diego <poupiot.diego@gmail.com>
2022-12-20 17:27:36 +0100

    Page détails

--- src/Controller/BienController.php
+++ src/Controller/BienController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\BienRepository;
+use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,9 +19,10 @@ class BienController extends AbstractController
     }
 
     #[Route('/bien/{id}', name: 'app_bien_show')]
-    public function show(BienRepository $bienRepository, int $id): Response
+    public function show(CategorieRepository $categorieRepository, BienRepository $bienRepository, int $id): Response
     {
         return $this->render('bien/show.html.twig', [
+            'categories' => $categorieRepository->findAll(),
             'bien' => $bienRepository->find($id),
         ]);
     }
