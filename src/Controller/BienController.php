8678763642d55ddd44264dd2268d5973939ce9f2
Sacha Solfer <sachafernandezsoltane@gmail.com>
2022-12-21 23:00:27 +0100

    Adding filter on page bien

--- src/Controller/BienController.php
+++ src/Controller/BienController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Bien;
+use App\Entity\Categorie;
 use App\Repository\BienRepository;
+use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +13,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class BienController extends AbstractController
 {
     #[Route('/bien', name: 'app_bien')]
-    public function index(BienRepository $bienRepository): Response
+    public function index(BienRepository $bienRepository, CategorieRepository $categorieRepository): Response
     {
-        return $this->render('bien/index.html.twig', [
-            'lesBiens' => $bienRepository->findAll(),
-        ]);
+
+        if (isset($_GET["Categ"]) && isset($_GET["Type"])) {
+            $id = $_GET["Categ"];
+            $type = $_GET["Type"];
+            if ($id == "") {
+                if ($type == "") {
+                    return $this->render('bien/index.html.twig', [
+                        'lesBiens' => $bienRepository->findAll(),
+                        'lesCateg' => $categorieRepository->findAll(),
+                    ]);
+                } else {
+                    return $this->render('bien/index.html.twig', [
+                        'lesBiens' => $bienRepository->findBy(['is_locatif' => $type]),
+                        'lesCateg' => $categorieRepository->findAll(),
+                    ]);
+                }
+            } else if ($type == "") {
+                return $this->render('bien/index.html.twig', [
+                    'lesBiens' => $bienRepository->findBy(['id_categorie' => $id]),
+                    'lesCateg' => $categorieRepository->findAll(),
+                ]);
+            } else {
+                return $this->render('bien/index.html.twig', [
+                    'lesBiens' => $bienRepository->findBy(['id_categorie' => $id, 'is_locatif' => $type]),
+                    'lesCateg' => $categorieRepository->findAll(),
+                ]);
+            }
+        } else {
+            return $this->render('bien/index.html.twig', [
+                'lesBiens' => $bienRepository->findAll(),
+                'lesCateg' => $categorieRepository->findAll(),
+            ]);
+        }
     }
 }
