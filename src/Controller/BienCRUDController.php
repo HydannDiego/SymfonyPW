bd9b2c329573cd3a286a2a666fc01426d195d5e7
Sacha Solfer <sachafernandezsoltane@gmail.com>
2022-12-29 15:15:18 +0100

    Fixing Bien issue when creating a new bien

--- src/Controller/BienCRUDController.php
+++ src/Controller/BienCRUDController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Bien;
 use App\Form\BienType;
 use App\Repository\BienRepository;
+use Exception;
+use PHPMailer\PHPMailer\PHPMailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -32,6 +34,32 @@ class BienCRUDController extends AbstractController
             $bienRepository->save($bien, true);
 
             return $this->redirectToRoute('app_bien_c_r_u_d_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+
+        try {
+            $mail = new PHPMailer;
+            $mail->isSMTP();                            // Set mailer to use SMTP
+            $mail->Host = 'smtp.mailtrap.io';           // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                     // Enable SMTP authentication
+            $mail->Username = 'b64dc9f9de43bb';       // SMTP username
+            $mail->Password = 'b8d29ee13ed6c6';         // SMTP password
+            $mail->Port = 2525;                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom('team.safer@safer.com', 'Mailer');
+            $mail->addAddress('test@gmail.com', 'Utilisateur');     //Add a recipient
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Un nouveau bien est disponible';
+            $mail->Body = 'Un nouveau bien est dispo ! ';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
 
         return $this->renderForm('bien_crud/new.html.twig', [
