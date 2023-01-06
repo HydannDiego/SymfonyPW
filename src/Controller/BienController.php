d304a212671123b0688e28b75e5c9a01a5ed55fd
Sacha Solfer <sachafernandezsoltane@gmail.com>
2023-01-07 00:57:15 +0100

    Changing filter

--- src/Controller/BienController.php
+++ src/Controller/BienController.php
@@ -13,39 +13,72 @@ class BienController extends AbstractController
     #[Route('/bien', name: 'app_bien')]
     public function index(BienRepository $bienRepository, CategorieRepository $categorieRepository): Response
     {
-        if (isset($_GET["Categ"]) && isset($_GET["Type"])) {
 
-            $id = $_GET["Categ"];
-            $type = $_GET["Type"];
+        if (!empty($_POST['Price'])) {
+            $price = $_POST['Price'];
+        } else {
+            $price = 999999;
+        }
+
+
+        if (isset($_POST["Categ"]) && isset($_POST["Type"]) && isset($_POST["local"])) {
+
+            $id = $_POST["Categ"];
+            $type = $_POST["Type"];
+            $local = $_POST["local"];
+
 
             $intitCateg = $categorieRepository->affichageIntit($id);
             $intitType = ($type == 0) ? 'Achat' : 'Location';
 
+
             if ($id == "") {
                 if ($type == "") {
-                    $lesBiens = $bienRepository->findAll();
-                    $titre = 'Affichage des biens';
+                    if ($local == "") {
+                        $lesBiens = $bienRepository->findAll();
+                        $titre = 'Affichage des biens';
+                    } else {
+                        $lesBiens = $bienRepository->findBy(['cp' => $local]);
+                        $titre = "Affichage Localisation : " . $local;
+                    }
                 } else {
-                    $lesBiens = $bienRepository->findBy(['is_locatif' => $type]);
-                    $titre = "Affichage Catégorie : " . $intitType;
+                    if ($local == "") {
+                        $lesBiens = $bienRepository->findBy(['is_locatif' => $type]);
+                        $titre = 'Affichage ' . $intitType;
+                    } else {
+                        $lesBiens = $bienRepository->findBy(['is_locatif' => $type, 'cp' => $local]);
+                        $titre = "Affichage " . $intitType . " à : " . $local;
+                    }
                 }
             } elseif ($type == "") {
-                $lesBiens = $bienRepository->findBy(['id_categorie' => $id, 'is_locatif' => $type]);
-                $titre = "Affichage Catégorie : " . $intitCateg[0]["intitule"];
+                if ($local == "") {
+                    $lesBiens = $bienRepository->findBy(['id_categorie' => $id, 'is_locatif' => $type]);
+                    $titre = "Affichage Catégorie : " . $intitCateg[0]["intitule"];
+                } else {
+                    $lesBiens = $bienRepository->findBy(['id_categorie' => $id, 'is_locatif' => $type, 'cp' => $local]);
+                    $titre = "Affichage : " . $intitCateg[0]["intitule"] . " à : " . $local;
+                }
             } else {
-                $lesBiens = $bienRepository->findBy(['id_categorie' => $id, 'is_locatif' => $type]);
-                $titre = "Affichage Catégorie : " . $intitCateg[0]["intitule"] . " et " . $intitType;
+                if ($local == "") {
+                    $lesBiens = $bienRepository->findBy(['id_categorie' => $id, 'is_locatif' => $type]);
+                    $titre = "Affichage : " . $intitCateg[0]["intitule"] . " en " . $intitType;
+                } else {
+                    $lesBiens = $bienRepository->findBy(['id_categorie' => $id, 'is_locatif' => $type, 'cp' => $local]);
+                    $titre = "Affichage Catégorie : " . $intitCateg[0]["intitule"] . " et " . $intitType . " à : " . $local;
+                }
+
             }
         } else {
             $lesBiens = $bienRepository->findAll();
             $titre = 'Affichage des biens';
-
         }
 
         return $this->render('bien/index.html.twig', [
             'lesBiens' => $lesBiens,
             'lesCateg' => $categorieRepository->findAll(),
+            'localisation' => $bienRepository->getLocalisation(),
             'titre' => $titre,
+            'prix' => $price,
         ]);
     }
 
