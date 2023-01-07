b68ba8c7ef7d608da92bc538f49dc89c6c45db31
Diego <poupiot.diego@gmail.com>
2023-01-07 22:12:02 +0100

    validations tests

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -12,9 +12,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Validation;
+
 
 class ContactController extends AbstractController
 {
+
     /**
      * Nous créons un nouvel objet de contact, créons un formulaire, gérons la réponse du formulaire, puis on renvoie le
      * formulaire
@@ -28,23 +33,50 @@ class ContactController extends AbstractController
     public function createAction(Request $request, EntityManagerInterface $entityManager): Response
     {
         $contact = new Contact;
+
         $form = $this->createForm(ContactFormType::class, $contact)
             ->add('nom', TextType::class, array('label' => 'Nom', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
             ->add('email', EmailType::class, array('label' => 'Email', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')));
+
+        $form->handleRequest($request);
+
         /*->add('Save', SubmitType::class, array('label'=> 'Submit', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-top:15px')))*/
 
         # Handle form response
-        $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $nom = $form['nom']->getData();
             $email = $form['email']->getData();
 
-            $contact->setNom($nom);
-            $contact->setEmail($email);
+            $validator = Validation::createValidator();
+            $violations = $validator->validate($nom, [
+                    new Length([
+                        'min' => 3,
+                        'max' => 20,
+                        'minMessage' => 'Your first name must be at least {{ limit }} characters long',
+                        'maxMessage' => 'Your first name cannot be longer than {{ limit }} characters',
+                    ]),
+                    new NotBlank([
+                        'message' => 'Please enter a first name',
+                    ]),
+                ]
+            );
 
-            $entityManager->persist($contact);
-            $entityManager->flush();
+            if (0 !== count($violations)) {
+                // there are errors, now you can show them
+                foreach ($violations as $violation) {
+                    echo $violation->getMessage().'<br>';
+                    $this->addFlash('notice', $violation->getMessage().'<br>');
+                }
+            } else {
+
+                $contact->setNom($nom);
+                $contact->setEmail($email);
+
+                $entityManager->persist($contact);
+                $entityManager->flush();
+                $this->addFlash('notice', 'Contact Saved');
+            }
 
             return $this->redirectToRoute("home");
         }
